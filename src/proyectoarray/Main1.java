9f55e02b17075fd0e783bc85527d8bba5be924c0
Esteban <esteban.ros@campusfp.es>
2021-01-25 10:59:51 +0100

    Snapshot 3

---
+++ src/proyectoarray/Main1.java
@@ -0,0 +1,13 @@
+
+package proyectoarray;
+
+public class Main1 {
+
+   
+    public static void main(String[] args) {
+        char[] character = {'H','G','T'};
+        String cadena = String.valueOf(character);
+        System.out.println(cadena);
+    }
+    
+}
