955ebd2efcfcfc9604740bb6a5b4cd517702313d
Esteban <esteban.ros@campusfp.es>
2021-01-25 10:00:44 +0100

    Snapshot 1

---
+++ src/proyectoarray/ProyectoArray.java
@@ -0,0 +1,16 @@
+package proyectoarray;
+
+public class ProyectoArray {
+
+
+    public static void main(String[] args) {
+        //Declaración del array   
+        String[] nombre = {"Luis","Miguel","Carlos"};
+           
+        //Recorrer el array
+        for(int i=0; i<nombre.length; i++){
+            System.out.println(nombre[i]);
+        }
+    }
+    
+}
